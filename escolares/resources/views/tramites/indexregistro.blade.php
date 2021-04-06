a632c339cf9bfd69eaddc10ed82ece4a3cb0264b
MICHELLE-ORTIZTERAN <1630395@upv.edu.mx>
2021-04-05 21:06:36 -0500

    sin funcionalidad

--- escolares/resources/views/tramites/indexregistro.blade.php
+++ escolares/resources/views/tramites/indexregistro.blade.php
@@ -60,32 +60,24 @@
               </div>
             </form>
             <form class="needs-validation" novalidate>
+              <div class="form-group">
+                <label for="">Alumno:</label>
+              </div>
               <div class="form-row">
                 <div class="col-md-4 mb-3">
-                  <label for="validationCustom01">First name</label>
-                  <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
-                  <div class="valid-feedback">
-                    Looks good!
-                  </div>
+                  <label for="">Fecha de Nacimiento</label>
+                  {{-- <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required> --}}
                 </div>
                 <div class="col-md-4 mb-3">
-                  <label for="validationCustom02">Last name</label>
-                  <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
-                  <div class="valid-feedback">
-                    Looks good!
-                  </div>
+                  <label for="">Edad</label>
+                  {{-- <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required> --}}
                 </div>
-                <div class="col-md-4 mb-3">
-                  <label for="validationCustomUsername">Username</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text" id="inputGroupPrepend">@</span>
-                    </div>
-                    <input type="text" class="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" required>
-                    <div class="invalid-feedback">
-                      Please choose a username.
-                    </div>
-                  </div>
+                <div class="col-md-4 mb-2">
+                  <label for="">Curp</label>
+                </div>
+                <div class="col-md-4 mb-2">
+                  <label for="">sexo</label>
+                  {{-- <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required> --}}
                 </div>
               </div>
               <div class="form-row">
